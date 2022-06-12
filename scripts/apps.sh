43eedb113c73f6e31c33a9a936a5d960e4f3ae1f
Paul Orlob <orlobpaul@gmail.com>
2022-06-12 20:08:22 +0200

    Changed scripts added setup

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -6,7 +6,7 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm manjaro-keyring
 sudo pacman -Suu --noconfirm
 
-sudo pacman -S --noconfirm git zsh yay base-devel alacritty flameshot nnn go micro stow
+sudo pacman -S --noconfirm git zsh yay base-devel alacritty flameshot nnn go micro stow glances
 
 yay -S --noconfirm google-chrome
 yay -S --noconfirm visual-studio-code-bin
@@ -19,7 +19,3 @@ git config --global user.email Paul Orlob
 git config --global core.editor micro
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions

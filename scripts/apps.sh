b58b89cd256276336c25e10a4b0bc6fe081f2f87
Paul Orlob <orlobpaul@gmail.com>
2022-06-12 12:44:06 +0200

    Added scripts

---
+++ scripts/apps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Using manjaro i3 minimal
+
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm manjaro-keyring
+sudo pacman -Suu --noconfirm
+
+sudo pacman -S --noconfirm git zsh yay base-devel alacritty flameshot nnn go micro stow
+
+yay -S --noconfirm google-chrome
+yay -S --noconfirm visual-studio-code-bin
+yay -S --noconfirm spotify
+yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k
+yay -S --noconfirm jetbrains-toolbox
+
+git config --global user.name orlobpaul@gmail.com
+git config --global user.email Paul Orlob
+git config --global core.editor micro
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
---
+++ scripts/stow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPTPATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+stow --dir "$SCRIPTPATH/../dotfiles/" --target ~ .

7ccd06c6563778fbc37b67c16a1e8be9a62cd298
Paul Orlob <orlobpaul@gmail.com>
2022-06-12 14:38:04 +0200

    Updated scripts and dotfiles

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -19,3 +19,7 @@ git config --global user.email Paul Orlob
 git config --global core.editor micro
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions

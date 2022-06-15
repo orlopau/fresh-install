a2694c83046ffca8a3996c11a0de8d80edc0e199
Paul Orlob <orlobpaul@gmail.com>
2022-06-15 16:45:01 +0200

    Removed dangerous adopt option

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-stow --dir "$SCRIPTPATH/../dotfiles/" --target ~ --adopt .
+stow --dir "$SCRIPTPATH/../dotfiles/" --target ~ .

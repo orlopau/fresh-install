c2d3dce2c96b3187debb5d81a2c18ec36a9cd03d
Paul Orlob <orlobpaul@gmail.com>
2022-06-15 16:22:57 +0200

    Added wallpaper

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,6 @@ source "$SCRIPTPATH/stow.sh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cd ~/Pictures
+wget --output-document=wallpaper.jpg https://unsplash.com/photos/DI0M_gafhcE/download\?ixid\=MnwxMjA3fDB8MXxzZWFyY2h8MTB8fGZvcmVzdCUyMGFlcmlhbHxlbnwwfDB8fHwxNjU1MzAyMTI0\&force\=true
